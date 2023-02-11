1385f3706c004c1ce3bc9c88f1b858514d02dc5c
Thib-numberX <peronnothibault@hotmail.fr>
2023-02-11 11:21:58 +0100

    html page about et BEM css

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import PlantView from '../views/PlantView.vue'
+import PlantView from '../views/PlantView.vue';
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/about',
+      name : 'about',
+      component : AboutView
     },
     {
       path: '/plant/:plantId(\\d+)', // on ne match que des chiffres
---
+++ src/views/AboutView.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="container-about">
+        <div class="container-about_info">
+            <img src="../../public/icons/face.svg" alt="icone représantant le développeur" class="about-info_icons_face">
+            <div>
+                <h1 class="about-info_h1">Thibault PERONNO</h1>
+                <p>Développeur Web et Web Mobile (Titre RNCP DWWM) et en cours de formation pour le titre de Concepteur et Développeur d'Application (Titre RNCP CDA)</p>
+            </div>
+        </div>
+        <div class="container-about_site">
+            <h3 class="about-site_h3">Pourqoi ce site en VueJs3 ?</h3>
+            <p>Ce site n'a vocation qu'à titre pédagogique sur le framework VueJS en version 3. Lors de la formation pour le titre de CDA au sein de l'école <a href="https://oclock.io/">O'Clock</a> nous sommes formés sur des tecnologique back-end et front-end. Cette année, c'est le framework <a href="https://vuejs.org/">VueJs</a> qui a été choisi.</p>
+            <p>Bien que la formation intensive nous donne les cartes pour devenir des développeurs employable, je souhaite approndir les techologie abordées lors de la formation. C'est pour cela, j'ai codé ce site.</p>
+            <div class="about-site_languages">
+                <h2>Langages du site</h2>
+                <img src="../../public/icons/vuejs.svg" alt="" class="icons_language">
+                <img src="../../public/icons/vitejs.svg" alt="" class="icons_language">
+                <img src="../../public/icons/css.svg" alt="" class="icons_language">
+        
+            </div>
+        </div>
+        <div class="container-about_languages">
+            <h3>Langages vue lors de mon parcours (formation et entreprise) en tant que développeur</h3>
+            <img src="../../public/icons/html.svg" alt="" class="icons_language">
+            <img src="../../public/icons/sass.svg" alt="" class="icons_language">
+            <img src="../../public/icons/javascript.svg" alt="" class="icons_language">
+            <img src="../../public/icons/php.svg" alt="" class="icons_language">
+            <img src="../../public/icons/react.svg" alt="" class="icons_language">
+            <img src="../../public/icons/redux.svg" alt="" class="icons_language">
+            <img src="../../public/icons/angular.svg" alt="" class="icons_language">
+            <img src="../../public/icons/laravel.svg" alt="" class="icons_language">
+            <img src="../../public/icons/docker.svg" alt="" class="icons_language">
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
